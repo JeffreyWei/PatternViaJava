2a1796a2da19c177595c839a9c39d152932f2e30
weij <weij@lhcis.com>
2023-02-20 16:07:14 +0800

    feature:add singleton

--- src/main/java/com/jeffrey/pattern/singleton/Singleton.java
+++ src/main/java/com/jeffrey/pattern/singleton/Singleton.java
@@ -6,4 +6,24 @@ package com.jeffrey.pattern.singleton;
  * @Description:
  */
 public class Singleton {
+	private static String INSTANCE = null;
+
+	public Singleton() {
+		if (null != INSTANCE) {
+			throw new IllegalStateException("该实例已经存在");
+		}
+	}
+
+	public static String getInstance() {
+		String instance = INSTANCE;
+		if (instance == null) {
+			synchronized (Singleton.class) {
+				instance = INSTANCE;
+				if (instance == null) {
+					instance = INSTANCE = "instancce";
+				}
+			}
+		}
+		return instance;
+	}
 }
