cbde22bd6a2590696faba5d96e595f7c11b3f93e
weij <weij@lhcis.com>
2023-03-05 15:45:39 +0800

    feature:add component

--- src/main/java/com/jeffrey/pattern/component/Client.java
+++ src/main/java/com/jeffrey/pattern/component/Client.java
@@ -9,10 +9,10 @@ package com.jeffrey.pattern.component;
  */
 public class Client {
 	public static void main(String[] args) {
-		Component com1=new Composite("root");
+		Component com1 = new Composite("root");
 		com1.add(new Leaf("A"));
 		com1.add(new Leaf("B"));
-		Component com2=new Composite("trunk");
+		Component com2 = new Composite("trunk");
 		com2.add(new Leaf(("C")));
 		com2.add(com1);
 		com2.display();
--- src/main/java/com/jeffrey/pattern/component/Leaf.java
+++ src/main/java/com/jeffrey/pattern/component/Leaf.java
@@ -6,25 +6,25 @@ package com.jeffrey.pattern.component;
  * @author weij
  */
 public class Leaf implements Component {
-    private String name;
-    private Component component;
+	private String name;
+	private Component component;
 
-    public Leaf(String name) {
-        this.name = name;
-    }
+	public Leaf(String name) {
+		this.name = name;
+	}
 
-    @Override
-    public void add(Component component) {
-        this.component = component;
-    }
+	@Override
+	public void add(Component component) {
+		this.component = component;
+	}
 
-    @Override
-    public void display() {
-        System.out.println("leaf name:" + this.name);
-    }
+	@Override
+	public void display() {
+		System.out.println("leaf name:" + this.name);
+	}
 
-    @Override
-    public void remove(Component component) {
-        this.component = null;
-    }
+	@Override
+	public void remove(Component component) {
+		this.component = null;
+	}
 }
