d19301290253cf68091c57c6c10bf68d27a5c861
weij <weij@lhcis.com>
2023-03-06 08:53:44 +0800

    feature:add decorate by file

--- src/main/java/com/jeffrey/pattern/decorate/fileimp/Client.java
+++ src/main/java/com/jeffrey/pattern/decorate/fileimp/Client.java
@@ -9,17 +9,17 @@ import java.net.URL;
  * Created by wei on 15/5/2.
  */
 public class Client {
-    public static void main(String[] args) {
-        try {
+	public static void main(String[] args) {
+		try {
 //            URL xmlConfig = Thread.currentThread().getContextClassLoader().getResource("logback.xml");
-            URL url = Thread.currentThread().getContextClassLoader().getResource("testFile");
-            InputStream is =new LowerCaseInputStream(new BufferedInputStream(new FileInputStream(url.getFile())));
-            int c;
-            while((c=is.read())>=0){
-                System.out.print((char)c);
-            }
-        }catch (Exception e){
-            System.out.println(e);
-        }
-    }
+			URL url = Thread.currentThread().getContextClassLoader().getResource("testFile");
+			InputStream is = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream(url.getFile())));
+			int c;
+			while ((c = is.read()) >= 0) {
+				System.out.print((char) c);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
