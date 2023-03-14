cc3ce01771ccd442b1c36a9a8969849aeab8b8ba
wei <mavenwei@qq.com>
2023-03-14 10:27:05 +0800

    feature:add factory method

--- src/main/java/com/jeffrey/pattern/factory/factorymethod/Client.java
+++ src/main/java/com/jeffrey/pattern/factory/factorymethod/Client.java
@@ -1,15 +1,15 @@
 package com.jeffrey.pattern.factory.factorymethod;
 
 /**
- *功能说明
+ * 功能说明
  *
  * @author weij
  */
 public class Client {
 
 	public static void main(String[] args) {
-		AnimalCreator animalCreator=new HumanCreator();
-		Animal animal=animalCreator.creatAnimal();
+		AnimalCreator animalCreator = new HumanCreator();
+		Animal animal = animalCreator.creatAnimal();
 		animal.identity();
 	}
 
