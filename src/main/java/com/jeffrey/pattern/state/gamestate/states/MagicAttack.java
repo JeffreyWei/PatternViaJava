eccdbfcae91a5e8e30eca44199555dad7df3b90e
wei <mavenwei@qq.com>
2023-04-13 09:07:24 +0800

    feature:add states

--- src/main/java/com/jeffrey/pattern/state/gamestate/states/MagicAttack.java
+++ src/main/java/com/jeffrey/pattern/state/gamestate/states/MagicAttack.java
@@ -6,9 +6,9 @@ import com.jeffrey.pattern.state.gamestate.*;
  * Created by wei on 15/5/5.
  */
 public class MagicAttack extends CharacterState {
-    @Override
-    public CharacterState handle(com.jeffrey.pattern.state.gamestate.Character character) {
-             return character.setState(new Normal());
+	@Override
+	public CharacterState handle(com.jeffrey.pattern.state.gamestate.Character character) {
+		return character.setState(new Normal());
 
-    }
+	}
 }
