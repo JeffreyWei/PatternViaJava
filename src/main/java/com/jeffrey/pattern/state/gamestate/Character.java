00aad29e1c0cbc6e8a4efdbbc4571cc341d113a5
wei <mavenwei@qq.com>
2023-04-17 09:00:34 +0800

    feature:add gamestate

--- src/main/java/com/jeffrey/pattern/state/gamestate/Character.java
+++ src/main/java/com/jeffrey/pattern/state/gamestate/Character.java
@@ -4,52 +4,52 @@ package com.jeffrey.pattern.state.gamestate;
  * Created by wei on 15/5/5.
  */
 public class Character {
-    private int HP;
-    private int MP;
-    private String name;
-    private CharacterState state;
+	private int HP;
+	private int MP;
+	private String name;
+	private CharacterState state;
 
-    public Character(String name, int HP, int MP) {
-        this.HP = HP;
-        this.MP = MP;
-        this.name = name;
-    }
+	public Character(String name, int HP, int MP) {
+		this.HP = HP;
+		this.MP = MP;
+		this.name = name;
+	}
 
-    public int getHP() {
-        return HP;
-    }
+	public int getHP() {
+		return HP;
+	}
 
-    public void setHP(int HP) {
-        this.HP = HP;
-    }
+	public void setHP(int HP) {
+		this.HP = HP;
+	}
 
-    public int getMP() {
-        return MP;
-    }
+	public int getMP() {
+		return MP;
+	}
 
-    public void setMP(int MP) {
-        this.MP = MP;
-    }
+	public void setMP(int MP) {
+		this.MP = MP;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public CharacterState getState() {
-        return state;
-    }
+	public CharacterState getState() {
+		return state;
+	}
 
-    public CharacterState setState(CharacterState state) {
-        this.state = state;
-        System.out.println("战斗报告--HP:" + this.HP + " MP:" + this.MP + " State:" + state.getClass().getName());
-        return state;
-    }
+	public CharacterState setState(CharacterState state) {
+		this.state = state;
+		System.out.println("战斗报告--HP:" + this.HP + " MP:" + this.MP + " State:" + state.getClass().getName());
+		return state;
+	}
 
-    public CharacterState startFight() {
-        return this.state.handle(this);
-    }
+	public CharacterState startFight() {
+		return this.state.handle(this);
+	}
 }
