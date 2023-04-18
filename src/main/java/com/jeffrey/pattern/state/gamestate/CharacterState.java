113ed0ccabbdad66de151fe46cc18d71e74cedcf
wei <mavenwei@qq.com>
2023-04-18 09:22:32 +0800

    feature:add state

--- src/main/java/com/jeffrey/pattern/state/gamestate/CharacterState.java
+++ src/main/java/com/jeffrey/pattern/state/gamestate/CharacterState.java
@@ -4,8 +4,8 @@ package com.jeffrey.pattern.state.gamestate;
  * Created by wei on 15/5/5.
  */
 public abstract class CharacterState {
-    //protected Character character;
-    public abstract CharacterState handle(Character character);
+	//protected Character character;
+	public abstract CharacterState handle(Character character);
 
 //    public Character getCharacter() {
 //        return character;
