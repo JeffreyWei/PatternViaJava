d028f596cedf3831758968d784b2fad523899467
wei <mavenwei@qq.com>
2023-04-19 08:54:24 +0800

    feature:add state

--- src/main/java/com/jeffrey/pattern/state/gamestate/Untils.java
+++ src/main/java/com/jeffrey/pattern/state/gamestate/Untils.java
@@ -4,7 +4,7 @@ package com.jeffrey.pattern.state.gamestate;
  * Created by wei on 15/5/5.
  */
 public class Untils {
-    public static int getRandom() {
-        return (int)((10)*Math.random()+1);
-    }
+	public static int getRandom() {
+		return (int) ((10) * Math.random() + 1);
+	}
 }
