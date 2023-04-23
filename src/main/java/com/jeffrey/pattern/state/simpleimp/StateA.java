5e83b7addee712c18289d3c023e0e63b19bc397c
wei <mavenwei@qq.com>
2023-04-23 08:54:28 +0800

    feature:add state

--- src/main/java/com/jeffrey/pattern/state/simpleimp/StateA.java
+++ src/main/java/com/jeffrey/pattern/state/simpleimp/StateA.java
@@ -9,8 +9,8 @@ public class StateA extends State {
 	@Override
 
 	public void handle(Context context) {
-			System.out.println("从状态A转到B");
-		StateB stateB=new StateB();
+		System.out.println("从状态A转到B");
+		StateB stateB = new StateB();
 		context.setState(stateB);
 	}
 }
